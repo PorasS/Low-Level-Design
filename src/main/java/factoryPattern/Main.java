806579eae1a3b904986c99850be18a7a392f0af3
Pratik Shende <pshende@tpnyc.local>
2023-12-20 01:01:36 +0530

    Factory Pattern

---
+++ src/main/java/factoryPattern/Main.java
@@ -0,0 +1,9 @@
+package factoryPattern;
+
+public class Main {
+    public static void main(String[] args) {
+        ShapeFactory shapeFactoryObj = new ShapeFactory();
+        Shape shapeObj = shapeFactoryObj.getShape("CIRCLE");
+        shapeObj.draw();
+    }
+}
