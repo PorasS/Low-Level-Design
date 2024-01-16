479c60e9ceb49998ac1dd9f4188c1c44f3305520
Pratik Shende <pshende@TINPNQ-LOM-0004.local>
2024-01-17 01:17:38 +0530

    Interpreter Pattern

--- src/main/java/solid/interpreterPattern/Client.java
+++ src/main/java/solid/interpreterPattern/Client.java
@@ -5,11 +5,21 @@ public class Client {
 
         Context context = new Context();
         context.put("a", 4);
-        context.put("b", 12);
+        context.put("b", 2);
 
         MultiplyNonTerminalExpression multiplyExpression = new MultiplyNonTerminalExpression(
                 new NumberTerminalExpression("a"), new NumberTerminalExpression("b"));
 
         System.out.println(multiplyExpression.interpret(context));
+
+        context.put("c", 6);
+        context.put("d", 5);
+
+        SumNonTerminalExpression sumExpression = new SumNonTerminalExpression(new MultiplyNonTerminalExpression(
+                new NumberTerminalExpression("a"), new NumberTerminalExpression("b")),
+                new MultiplyNonTerminalExpression(new NumberTerminalExpression("c"),
+                        new NumberTerminalExpression("d")));
+
+        System.out.println(sumExpression.interpret(context));
     }
 }
